ffc6fbbe7411df089e07e3c133a2841500b6e58c
Pablo Toharia <pablo.toharia@urjc.es>
2014-04-09 16:40:15 +0200

    Column and Minicolumn added

---
+++ src/Column.h
@@ -0,0 +1,27 @@
+/**
+ * @file    Column.h
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date    
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved. Do not distribute without further notice.
+ */
+#ifndef __NS_DATA_LIB_COLUMN_
+#define __NS_DATA_LIB_COLUMN_
+
+#include <MiniColumn.h>
+
+namespace nsdl {
+  
+  class Column {
+ 
+  protected:
+
+    Vector<Column> _columns;
+    
+
+  };
+  
+}
+
+
+#endif
---
+++ src/MiniColumn.h
@@ -0,0 +1,27 @@
+/**
+ * @file    MiniColumn.h
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date    
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved. Do not distribute without further notice.
+ */
+#ifndef __NS_DATA_LIB_MINICOLUMN_
+#define __NS_DATA_LIB_MINICOLUMN_
+
+#include <Neuron.h>
+
+namespace nsdl {
+  
+  class MiniColumn {
+ 
+  protected:
+
+    Vector<Neuron> _neurons;
+    
+
+  };
+  
+}
+
+
+#endif
