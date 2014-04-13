7464837457237b439f6b6b2ab5888dfc82936ff6
Pablo Toharia <pablo.toharia@urjc.es>
2014-04-13 23:11:43 +0200

    Added .gitignore and Dendrites container class

---
+++ src/Dendrites.h
@@ -0,0 +1,21 @@
+/**
+ * @file    Dendrites.h
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date    
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved. Do not distribute without further notice.
+ */
+#ifndef __NS_DATA_LIB_DENDRITES__
+#define __NS_DATA_LIB_DENDRITES__
+
+#include <Types.h>
+#include <Dendrite.h>
+
+namespace nsdl {
+
+  class Dendrites : public Vector<DendritePtr> {
+  };
+
+}
+
+#endif
