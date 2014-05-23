45c8f23cdf1ddde110db526818770248b56f60e1
Pablo Toharia <pablo.toharia@urjc.es>
2014-05-23 12:07:37 +0200

    Added headers for volume computation. Fixed CMakeLists install rule

--- src/Neuron.h
+++ src/Neuron.h
@@ -2,7 +2,7 @@
  * @file    Neuron.h
  * @brief
  * @author  Pablo Toharia <pablo.toharia@urjc.es>
- * @date    
+ * @date
  * @remarks Copyright (c) GMRV/URJC. All rights reserved. Do not distribute without further notice.
  */
 #ifndef __NOL_NEURON_
@@ -15,132 +15,186 @@
 #include <Container/Dendrites.h>
 #include <Container/Neurites.h>
 
-namespace nol {
+namespace nol
+{
 
 
   /*! \class Neuron
     \brief A class to represent neurons.
-    
+
     TODO: extend long description
-  */  
-  class Neuron {
+  */
+  class Neuron
+  {
 
-  public:
-    
-    /**
-     * Default Neuron class constructor.
-     * TODO: construct protected objects
-     */
-    Neuron() {}
+    public:
 
-    /**
-       * Method to add a new neurite to the neuron.
-       * @param neuriteType type of Neurite.
-       * @see Neurite::TNeuriteType
-       * @return pointer to the added neurite
+      /**
+       * Default Neuron class constructor.
+       * TODO: construct protected objects
        */
-    NeuritePtr addNeurite(Neurite::TNeuriteType neuriteType = 
-			  Neurite::DENDRITE) {
-      if (neuriteType == Neurite::DENDRITE)
-	_neurites.push_back(new Dendrite());
-      else if (neuriteType == Neurite::AXON)
-	_neurites.push_back(new Axon());
-      else
-	return NULL;
-      return  _neurites.back();
-    };
+      Neuron() {}
 
-    /**
-       * Method to add a new dendrite to the neuron.
-       * @param dendriteType type of Dendrite
-       * @see ndsl::Dendrite::TDendriteType
-       * @return pointer to the added dendrite
-       */
-    Dendrite * addDendrite(Dendrite::TDendriteType dendriteType = 
-			   Dendrite::BASAL) {
-      _neurites.push_back(new Dendrite(dendriteType));
-      return _neurites.back()->asDendrite();
-    };
+      /**
+         * Method to add a new neurite to the neuron.
+         * @param neuriteType type of Neurite.
+         * @see Neurite::TNeuriteType
+         * @return pointer to the added neurite
+         */
+      NeuritePtr addNeurite(Neurite::TNeuriteType neuriteType =
+                              Neurite::DENDRITE)
+      {
+        if (neuriteType == Neurite::DENDRITE)
+          _neurites.push_back(new Dendrite());
+        else if (neuriteType == Neurite::AXON)
+          _neurites.push_back(new Axon());
+        else
+          return NULL;
 
-    unsigned int numNeurites(void) {
-      return _neurites.size();
-    }
+        return  _neurites.back();
+      };
 
-    unsigned int numDendrites(void) {
-      unsigned int nd = 0;
-      for (Vector<Neurite *>::iterator it = _neurites.begin(); 
-	   it != _neurites.end(); ++it) 
-	if ((*it)->asDendrite()) nd++;
-      return nd;
-    }
+      /**
+         * Method to add a new dendrite to the neuron.
+         * @param dendriteType type of Dendrite
+         * @see ndsl::Dendrite::TDendriteType
+         * @return pointer to the added dendrite
+         */
+      Dendrite *addDendrite(Dendrite::TDendriteType dendriteType =
+                              Dendrite::BASAL)
+      {
+        _neurites.push_back(new Dendrite(dendriteType));
+        return _neurites.back()->asDendrite();
+      };
 
-    /**
-       * Method to get all the dendrites in a container. 
-       * Memory for the container is allocated.
-       * @return pointer to the container of Dendrites
-       */
-    Dendrites * dendrites(void) {
-      Dendrites *dendrites = new Dendrites;
-      for (Vector<Neurite *>::iterator it = _neurites.begin(); 
-	   it != _neurites.end(); ++it) 
-	if ((*it)->asDendrite())
-	  dendrites->push_back((*it)->asDendrite());      
-      return dendrites;
-    }
-    
-    /**
-       * Method to get all the basal dendrites in a container. 
-       * Memory for the container is allocated.
-       * @return pointer to the container of Dendrites
-       */
-    Dendrites * basalDendrites(void) {
-      // TODO: create a list of all basal dendrites
-      return new Dendrites;
-    }
-
-    /**
-       * Method to add a new dendrite to the neuron.
-       * @param dendriteType  .
-       * @see Neurite::TNeuriteType
-       * @return pointer to the added dendrite
-       */
-    Dendrite * apicalDendrite(void) {
-      for (Vector<Neurite *>::iterator it = _neurites.begin(); 
-	   it != _neurites.end(); ++it) {
-	if ((*it)->asDendrite() && 
-	    ((*it)->asDendrite()->dendriteType() == Dendrite::APICAL))
-	  return (*it)->asDendrite();
+      unsigned int numNeurites(void)
+      {
+        return _neurites.size();
       }
-      return NULL;
-    }
-    
-    Neurites & neurites(void) {
-      return _neurites;
-    }
 
-    
-    Soma & soma(void) {
-      return _soma;
-    }
+      unsigned int numDendrites(void)
+      {
+        unsigned int nd = 0;
 
-  
-  protected:
-    
-    Soma _soma; 
-    //    Vector<Neurite *> _neurites;
-    Neurites _neurites;
-    
+        for (Vector<Neurite *>::iterator it = _neurites.begin();
+             it != _neurites.end(); ++it)
+          if ((*it)->asDendrite()) nd++;
+
+        return nd;
+      }
+
+      unsigned int numNeuriteBranches () {
+        // TODO
+    	  return 0;
+      }
+
+      unsigned int numDendriteBranches () {
+        // TODO
+    	  return 0;
+      }
+
+      unsigned int numAxonBranches () {
+        // TODO
+    	return 0;
+      }
+
+      float volume() {
+        //TODO
+      	return this->neuritesVolume() + _soma.volume();
+      }
+
+      float neuritesVolume() {
+    	  //TODO
+    	  return this->dendritesVolume() + this->axonVolume();
+      }
+
+      float dendritesVolume() {
+    	  //TODO
+    	  return 0.0f;
+      }
+
+      float axonVolume() {
+    	  //TODO
+    	  return 0.0f;
+      }
+
+
+      /**
+         * Method to get all the dendrites in a container.
+         * Memory for the container is allocated.
+         * @return pointer to the container of Dendrites
+         */
+      Dendrites *dendrites(void)
+      {
+        Dendrites *dendrites = new Dendrites;
+
+        for (Vector<Neurite *>::iterator it = _neurites.begin();
+             it != _neurites.end(); ++it)
+          if ((*it)->asDendrite())
+            dendrites->push_back((*it)->asDendrite());
+
+        return dendrites;
+      }
+
+      /**
+         * Method to get all the basal dendrites in a container.
+         * Memory for the container is allocated.
+         * @return pointer to the container of Dendrites
+         */
+      Dendrites *basalDendrites(void)
+      {
+        // TODO: create a list of all basal dendrites
+        return new Dendrites;
+      }
+
+      /**
+         * Method to add a new dendrite to the neuron.
+         * @param dendriteType  .
+         * @see Neurite::TNeuriteType
+         * @return pointer to the added dendrite
+         */
+      Dendrite *apicalDendrite(void)
+      {
+        for (Vector<Neurite *>::iterator it = _neurites.begin();
+             it != _neurites.end(); ++it)
+        {
+          if ((*it)->asDendrite() &&
+              ((*it)->asDendrite()->dendriteType() == Dendrite::APICAL))
+            return (*it)->asDendrite();
+        }
+
+        return NULL;
+      }
+
+      Neurites &neurites(void)
+      {
+        return _neurites;
+      }
+
+
+      Soma &soma(void)
+      {
+        return _soma;
+      }
+
+
+    protected:
+
+      Soma _soma;
+      //    Vector<Neurite *> _neurites;
+      Neurites _neurites;
+
 
   };
 
-/*   //! Definition of pointer to Dendrite */
-/* #if !BOOST */
-/*   typedef  Neuron * NeuronPtr; */
-/* #else */
-/*   typedef  boost::shared_ptr<Neuron> * NeuronPtr; */
-/* #endif */
-  
-  
+  /*   //! Definition of pointer to Dendrite */
+  /* #if !BOOST */
+  /*   typedef  Neuron * NeuronPtr; */
+  /* #else */
+  /*   typedef  boost::shared_ptr<Neuron> * NeuronPtr; */
+  /* #endif */
+
+
 }
 
 
