f9d1d73bac75a2b676365b2b5e52c9ee9f665a2f
Pablo Toharia <pablo.toharia@urjc.es>
2014-12-05 09:50:32 +0100

    Added Factory.h

---
+++ nsol/Factory.h
@@ -0,0 +1,103 @@
+/**
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved.
+ *          Do not distribute without further notice.
+ */
+#ifndef __NSOL_FACTORY__
+#define __NSOL_FACTORY__
+
+#include <nsol/nsol.h>
+
+namespace nsol
+{
+
+#define FACTORY_TEMPLATE_CLASSES                 \
+    class NODE,                                  \
+    class SEGMENT,                               \
+    class SECTION,                               \
+    class DENDRITE,                              \
+    class AXON,                                  \
+    class SOMA,                                  \
+    class NEURONMORPHOLOGY,                      \
+    class NEURON,                                \
+    class MINICOLUMN,                            \
+    class COLUMN
+
+#define FACTORY_TEMPLATE_CLASS_NAMES            \
+    NODE,                                       \
+    SEGMENT,                                    \
+    SECTION,                                    \
+    DENDRITE,                                   \
+    AXON,                                       \
+    SOMA,                                       \
+    NEURONMORPHOLOGY,                           \
+    NEURON,                                     \
+    MINICOLUMN,                                 \
+    COLUMN
+
+  namespace generic
+  {
+    template < FACTORY_TEMPLATE_CLASSES >
+    class Factory
+    {
+
+    public:
+
+      static NeuronPtr newNeuron( bool createMorphology = true )
+      {
+        if ( createMorphology )
+          return NeuronPtr (
+            new NEURON( new NEURONMORPHOLOGY( new SOMA )));
+        else
+          return NeuronPtr ( new NEURON );
+      }
+
+
+      static NeuronMorphologyPtr newNeuronMorphology( void )
+      {
+        return new NEURONMORPHOLOGY( new SOMA );
+      }
+
+    };
+  } // namespace generic
+
+  typedef generic::Factory< Node,
+                            Segment,
+                            Section,
+                            Dendrite,
+                            Axon,
+                            Soma,
+                            NeuronMorphology,
+                            Neuron,
+                            MiniColumn,
+                            Column > Factory;
+
+  typedef generic::Factory< Node,
+                            SegmentStats,
+                            SectionStats,
+                            DendriteStats,
+                            AxonStats,
+                            SomaStats,
+                            NeuronMorphologyStats,
+                            Neuron,
+                            MiniColumnStats,
+                            ColumnStats > FactoryStats;
+
+  typedef generic::Factory< NodeCached,
+                            SegmentCachedStats,
+                            SectionCachedStats,
+                            DendriteCachedStats,
+                            AxonCachedStats,
+                            SomaStats,
+                            NeuronMorphologyCachedStats,
+                            Neuron,
+                            MiniColumnStats,
+                            ColumnStats > FactoryCachedStats;
+
+
+
+} // namespace nsol
+
+#endif // __NSOL_FACTORY__
