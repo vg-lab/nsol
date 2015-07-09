084435d13ecc6bd45a39ab1703485b81611bf24e
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2015-07-09 15:51:06 +0200

    DataSet: added functionality to load a neuron from swc format

--- nsol/DataSet.h
+++ nsol/DataSet.h
@@ -13,6 +13,7 @@
 #include "NsolTypes.h"
 #include "Container/Columns.h"
 #include "Reader/BBPSDKReader.h"
+#include "Reader/SwcReader.h"
 
 namespace nsol
 {
@@ -54,6 +55,50 @@ namespace nsol
     void close( void );
 
     Columns& columns( void );
+
+    template < class NODE = Node,
+               class SEGMENT = Segment,
+               class SECTION = Section,
+               class DENDRITE = Dendrite,
+               class AXON = Axon,
+               class SOMA = Soma,
+               class NEURONMORPHOLOGY = NeuronMorphology,
+               class NEURON = Neuron >
+    void addNeuron( const std::string& swc, unsigned int columnId_,
+        unsigned int miniColumnId_/*,
+        Matrix4_4f transform_, unsigned int gid_
+        unsigned int layer_*/ )
+    {
+      SwcReaderTemplated< NODE, SEGMENT, SECTION, DENDRITE, AXON, SOMA,
+          NEURONMORPHOLOGY, NEURON > swcReader;
+      NeuronPtr neuron = swcReader.readNeuron( swc );
+
+      /*
+       * neuron->gid( gid_ );
+       * neuron->layer( layer_ );
+       * neuron->transform( transform_ );
+       *
+       */
+
+      if ( _columns.size( ) <= columnId_ )
+      {
+        for ( unsigned int i = _columns.size() ; i < columnId_ + 1; i++ )
+        {
+          _columns.push_back( new Column( ));
+        }
+      }
+
+      MiniColumns miniColumns = _columns[ columnId_ ]->miniColumns( );
+      if ( miniColumns.size( ) <= miniColumnId_ )
+      {
+        for ( unsigned int i = miniColumns.size() ; i < miniColumnId_ + 1; i++ )
+        {
+          miniColumns.push_back( new MiniColumn( ));
+        }
+      }
+
+      miniColumns[ miniColumnId_ ]->addNeuron( neuron );
+    }
 
   protected:
 
