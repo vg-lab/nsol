a8bb154136461e2523c83775cb8e9b912653cbfb
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2015-07-16 18:20:35 +0200

    Defautl neuron type when loading scene set to undefined

--- nsol/DataSet.h
+++ nsol/DataSet.h
@@ -247,7 +247,7 @@ namespace nsol
                                 attributes.value( "gid" ).toUInt( );
                             unsigned int layer = 1;
                             nsol::Neuron::TNeuronType type =
-                                nsol::Neuron::PYRAMIDAL;
+                                nsol::Neuron::UNDEFINED;
                             Matrix4_4f transform = Matrix4_4f::IDENTITY;
 
                             if ( attributes.hasAttribute( "layer" ))
@@ -265,6 +265,12 @@ namespace nsol
                               else if( typeString == "PYRAMIDAL" )
                               {
                                 type = nsol::Neuron::PYRAMIDAL;
+                              }
+                              else
+                              {
+                                NSOL_LOG( std::string( "Neuron " ) +
+                                    std::to_string( gid ) +
+                                    std::string( " undefined type." );
                               }
                             }
                             // Looking for transform in neuron
