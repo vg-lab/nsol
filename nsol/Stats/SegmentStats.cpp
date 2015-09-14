59f18ed670299cc69547f0d552f1f13cb94b661c
r.jarillo <r.jarillo@alumnos.urjc.es>
2015-09-14 13:08:19 +0200

    Creating class SegmentStats to compute stats for segments.

---
+++ nsol/Stats/SegmentStats.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file    SegmentStats.cpp
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved.
+ *          Do not distribute without further notice.
+ */
+
+#include "SegmentStats.h"
+#include "../error.h"
+
+namespace nsol
+{
+
+  SegmentStats * SegmentStats::stats( void )
+  {
+    return this;
+  }
+
+  float SegmentStats::getStat( TSegmentStat stat,
+							   const NodePtr begin,
+							   const NodePtr end  ) const
+  {
+	  switch ( stat )
+	      {
+	      case SURFACE:
+	      return ( begin && end) ?
+	        float( M_2PI) * end->radius( ) *
+	        ( begin->point( ) - end->point( )).length( ) :
+	        0.0f;
+	        break;
+
+	      case VOLUME:
+	      return ( begin && end) ?
+	        float( M_PI) *
+	        ( begin->point( ) - end->point( )).length( ) *
+	        end->radius( ) *
+	        end->radius( ) : 0.0f;
+	        break;
+
+	      case LENGTH:
+	      return ( begin && end) ?
+	        ( begin->point( ) - end->point( )).length( ) : 0.0f;
+	        break;
+
+	      default:
+	        NSOL_THROW( "stat not implemented for SegmentStats" );
+	      }
+	      return 0.0f;
+  } // class SegmentStats
+
+} // namespace nsol
+
---
+++ nsol/Stats/SegmentStats.h
@@ -0,0 +1,48 @@
+/**
+ * @file    SegmentStats.cpp
+ * @brief
+ * @author  Pablo Toharia <pablo.toharia@urjc.es>
+ * @date
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved.
+ *          Do not distribute without further notice.
+ */
+
+#ifndef __NSOL_SEGMENT_STATS__
+#define __NSOL_SEGMENT_STATS__
+
+#include <nsol/api.h>
+#include "NsolTypes.h"
+#include "Node.h"
+
+namespace nsol
+{
+
+	class SegmentStats
+	{
+
+	public:
+
+		//! Stats that can be computed for a segment
+		typedef enum
+		{
+		  SURFACE = 0,
+		  VOLUME,
+		  LENGTH,
+		  SEGMENT_NUM_STATS
+		} TSegmentStat;
+
+
+		NSOL_API
+		SegmentStats * stats( void );
+
+		NSOL_API
+		static virtual float getStat( TSegmentStat stat, const NodePtr begin,
+												  const NodePtr end) const;
+
+
+	}; // class SegmentStats
+
+
+} // namespace nsol
+
+#endif // __NSOL_SEGMENT_STATS__
