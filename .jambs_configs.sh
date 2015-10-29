5e0d1fc0ca6f27ec66ab49e0cded6f9f0002ccd9
Pablo Toharia <pablo.toharia@urjc.es>
2015-10-29 15:49:56 +0100

    Renamed .jambs_configs to .jambs_configs.sh and added to it tags of dependent projects.

---
+++ .jambs_configs.sh
@@ -0,0 +1,6 @@
+git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
+git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
+git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
+export JAMBS_vmmlib_repo_tag=67c1035
+export JAMBS_bbpsdk_repo_tag=0ce707b
+export JAMBS_fires_repo_tag=1324ed4
