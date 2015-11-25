6e6121e78966886b390cfe99b668486a5aba5f7c
Pablo Toharia <pablo.toharia@urjc.es>
2015-11-25 13:11:00 +0100

    Updated tag dependencies.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -2,5 +2,7 @@ git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
 git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
 export JAMBS_vmmlib_repo_tag=67c1035
+export JAMBS_lunchbox_repo_tag=306cd80
+export JAMBS_brion_repo_tag=015515f
 export JAMBS_bbpsdk_repo_tag=0ce707b
 export JAMBS_fires_repo_tag=1324ed4
