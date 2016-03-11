8bb9b4ce422ded85a108fa0d9ea1e70dfdb60f93
Pablo Toharia <pablo.toharia@upm.es>
2016-03-11 16:37:35 +0100

    Bump to jambs config tags.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -1,8 +1,8 @@
 git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
 git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
-export JAMBS_vmmlib_repo_tag=67c1035
-export JAMBS_lunchbox_repo_tag=306cd80
-export JAMBS_brion_repo_tag=015515f
-export JAMBS_bbpsdk_repo_tag=0ce707b
-export JAMBS_fires_repo_tag=1324ed4
+export JAMBS_vmmlib_repo_tag=d23b59d
+export JAMBS_lunchbox_repo_tag=d26906e
+export JAMBS_brion_repo_tag=5c2ef23
+export JAMBS_bbpsdk_repo_tag=bcd48b4
+export JAMBS_fires_repo_tag=c2b4d62
