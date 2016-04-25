f40927038a431d52a71b64ecc37fe1568602d9c9
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2016-04-25 18:03:00 +0200

    Added class Simplifier: this class simplify NeuronMorphologies modifying their sections

---
+++ nsol/Simplifier.cpp
@@ -0,0 +1,91 @@
+#include "Simplifier.h"
+
+#include "NeuronMorphology.h"
+#include "Container/Nodes.h"
+
+namespace nsol
+{
+
+  Simplifier* Simplifier::m_pInstance = NULL;
+
+  Simplifier* Simplifier::Instance( void )
+  {
+    if ( !m_pInstance )
+      m_pInstance = new Simplifier;
+
+    return m_pInstance;
+  }
+
+  NeuronMorphologyPtr Simplifier::simplify( NeuronMorphologyPtr morpho_,
+                                            TSimplificationMethod simplMethod_,
+                                            float tolerance_,
+                                            bool clone )
+  {
+    NeuronMorphologyPtr morpho = morpho_;
+
+    if ( clone )
+      morpho = morpho->clone( );
+
+    typedef void (*func)( SectionPtr, float ) ;
+
+    func function = nullptr;
+
+    switch( simplMethod_ )
+    {
+    case 0:
+      std::cout << "Simplification method: Delete all" << std::endl;
+      function = Simplifier::_simplSecDeleteAll;
+      break;
+    case 1:
+      std::cout << "Simplification method: Distance between nodes" << std::endl;
+      function = Simplifier::_simplSecDistNodes;
+      break;
+    default:
+      std::cout << "Simplification method: Delete all" << std::endl;
+      function = Simplifier::_simplSecDeleteAll;
+      break;
+    }
+
+    if ( !function )
+      return morpho;
+
+    for ( auto neurite: morpho->neurites( ))
+      for ( auto section: neurite->sections( ))
+      {
+        std::cout << "simplificando seccion" << std::endl;
+
+        function( section, tolerance_ );
+      }
+    return morpho;
+  }
+
+  void Simplifier::_simplSecDeleteAll( SectionPtr section_,
+                                       float /*tolerance_*/ )
+  {
+    Nodes* nodes = &section_->nodes( );
+    for ( unsigned int i = 1; i < nodes->size( )-1; i++ )
+    {
+      delete (*nodes)[i];
+      nodes->erase( nodes->begin( ) + i );
+      i--;
+    }
+    // nodes->erase( nodes->begin( )+1, nodes->end( )-1);
+  }
+
+  void Simplifier::_simplSecDistNodes( SectionPtr section_,
+                                       float tolerance_ )
+  {
+    Nodes* nodes = &section_->nodes( );
+    for ( unsigned int i = 1; i < nodes->size( )-1; i++ )
+    {
+      float distance =
+        ((*nodes)[i-1]->point( ) - (*nodes)[i]->point( )).length( );
+      if ( distance < tolerance_ )
+      {
+        delete (*nodes)[i];
+        nodes->erase( nodes->begin( ) + i );
+        i--;
+      }
+    }
+  }
+} // namespace nsol
---
+++ nsol/Simplifier.h
@@ -0,0 +1,65 @@
+/**
+ * @file Simplifier.h
+ * @brief
+ * @author Juan Jose Garcia Cantero <juanjose.garcia@urjc.es>
+ * @date
+ * @remarks Copyright (c) GMRV/URJC. All rights reserved.
+ *          Do not distribute without further notice.
+ */
+
+#ifndef __NSOL_SIMPLIFIER__
+#define __NSOL_SIMPLIFIER__
+
+#include <nsol/api.h>
+
+#include "NsolTypes.h"
+
+namespace nsol
+{
+
+  class Simplifier
+  {
+
+  public:
+
+    //! Methods of simplification
+    typedef enum
+    {
+      DELETE_ALL = 0,
+      DIST_NODES
+    } TSimplificationMethod;
+
+    NSOL_API
+    static Simplifier* Instance( );
+
+    NSOL_API
+    NeuronMorphologyPtr simplify(
+      NeuronMorphologyPtr morpho_,
+      TSimplificationMethod simplMethod_ = DELETE_ALL,
+      float tolerance_ = 0.1,
+      bool clone = false );
+
+    NSOL_API
+    Simplifier( Simplifier const& ) = delete;
+
+    NSOL_API
+    void operator = ( Simplifier const& ) = delete;
+
+  private:
+
+    Simplifier( void ){ };
+
+    static void _simplSecDeleteAll( SectionPtr section_,
+                                    float tolerance_ );
+
+    static void _simplSecDistNodes( SectionPtr section_,
+                                    float tolerance_ );
+
+    static Simplifier* m_pInstance;
+  };
+
+} // namespace nsol
+
+#endif // __NSOL_SIMPLIFIER__
+
+// EOF
