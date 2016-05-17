326af6805c3bc038e07f79109bf5b7873f32c747
Juan Jose Garcia <juanjosegarciacan@gmail.com>
2016-05-17 15:37:51 +0200

    Simplifier class documentation

--- nsol/Simplifier.h
+++ nsol/Simplifier.h
@@ -17,6 +17,12 @@
 namespace nsol
 {
 
+  /*! \class Simplifier
+    \brief A singleton class to operate over morphologies
+
+    Provides functionality to the simplification, the adaptation
+    and the cutout of neuronal morphologies
+  */
   class Simplifier
   {
 
@@ -29,9 +35,23 @@ namespace nsol
       DIST_NODES,
     } TSimplificationMethod;
 
+    /**
+     * Returns the instance of the singelton class
+     * @return the Simplifier instance
+     */
     NSOL_API
     static Simplifier* Instance( );
 
+    /**
+     * Method to simplify the morphology given by the simplification function
+     * selected, the simplification can be made over the original given
+     * morphology or over a clone of this morphology
+     * @param morpho_ neuronal morphology to simplify
+     * @param simplMethod_ to select the function used to simplify
+     * @param tolerance_ used for different simplification function
+     * @param clone if true the morphology is clone before the simplification
+     *        if false the simplification is made over the orignal morphology
+     */
     NSOL_API
     NeuronMorphologyPtr simplify(
       NeuronMorphologyPtr morpho_,
@@ -39,11 +59,26 @@ namespace nsol
       float tolerance_ = 0.1,
       bool clone = false );
 
+
+    /**
+     * Mehod to correct the neuronal morphology to avoid errors in the
+     * generation of the neuronal soma mesh
+     * @param morpho_ neuronal morphology to correct
+     * @param clone if true the morphology is clone before the correction
+     *        if false the correction is made over the orignal morphology
+     */
     NSOL_API
     NeuronMorphologyPtr adaptSoma(
       NeuronMorphologyPtr morpho_,
       bool clone = false );
 
+
+    /**
+     * Mehod to cutout the neuronal morphology based in the nodes importances
+     * @param morpho_ neuronal morphology to cutout
+     * @param clone if true the morphology is clone before the cutout
+     *        if false the cutout is made over the orignal morphology
+     */
     NSOL_API
     NeuronMorphologyPtr cutout(
       NeuronMorphologyPtr morpho_,
@@ -63,12 +98,18 @@ namespace nsol
 
     void _cutoutSection( SectionPtr section_ );
 
+
+    /** @name Simplification functions */
+    ///@{
+
     static void _simplSecDeleteAll( SectionPtr section_,
                                     float tolerance_ );
 
     static void _simplSecDistNodes( SectionPtr section_,
                                     float tolerance_ );
+    ///@}
 
+    //! Static instance of Simplifier singleton class
     static Simplifier* m_pInstance;
   };
 
