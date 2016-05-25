f2bd267b32074c15e548839a03f0bf517d2c4233
Pablo Toharia <pablo.toharia@urjc.es>
2016-05-25 12:18:51 +0200

    Updated cmake/common and deps tags.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -1,7 +1,7 @@
 git clone git@gitlab.gmrv.es:common/jambs-config-nsviz.git
 git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
-export JAMBS_lunchbox_repo_tag=d26906e
-export JAMBS_brion_repo_tag=5c2ef23
-export JAMBS_bbpsdk_repo_tag=bcd48b4
-export JAMBS_fires_repo_tag=c2b4d62
+export JAMBS_lunchbox_repo_tag=b990d7f
+export JAMBS_brion_repo_tag=aead60c
+#export JAMBS_bbpsdk_repo_tag=bcd48b4
+export JAMBS_fires_repo_tag=1932f08
