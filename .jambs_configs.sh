a6cf36360d7c3f13bd7e79e07b799540bb91086f
Pablo Toharia <pablo.toharia@urjc.es>
2016-05-25 13:14:32 +0200

    Bump fires.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -4,4 +4,4 @@ git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
 export JAMBS_lunchbox_repo_tag=b990d7f
 export JAMBS_brion_repo_tag=aead60c
 #export JAMBS_bbpsdk_repo_tag=bcd48b4
-export JAMBS_fires_repo_tag=1932f08
+export JAMBS_fires_repo_tag=b007eee
