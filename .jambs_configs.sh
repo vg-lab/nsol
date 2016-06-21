4e93393efd8d71577f5e4dccf31ce2a8cbbdb481
Pablo Toharia <pablo.toharia@urjc.es>
2016-06-21 20:28:27 +0200

    Bump Brion.

--- .jambs_configs.sh
+++ .jambs_configs.sh
@@ -3,6 +3,6 @@ git clone git@gitlab.gmrv.es:common/jambs-config-hbp.git
 git clone git@gitlab.gmrv.es:common/jambs-config-retrieval.git
 
 export JAMBS_lunchbox_repo_tag=b990d7f
-export JAMBS_brion_repo_tag=aead60c
+export JAMBS_brion_repo_tag=2bb27a1
 export JAMBS_bbpsdk_repo_tag=aff71637
 export JAMBS_fires_repo_tag=b007eee
